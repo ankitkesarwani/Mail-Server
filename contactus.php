e24b0d978aef96239f02800ca75e58726ad4989d
Ankit kesarwani <kesarwani.ankit48@gmail.com>
2017-09-07 22:18:58 +0530

    Files

---
+++ contactus.php
@@ -0,0 +1,30 @@
+<style>
+	table{padding:5px;border-radius:5px}
+	td{padding:10px}
+</style>
+<table width="90%" border="1" align="center">
+  <tr style="background:#CCCCCC" height="30px">
+    <th colspan="2" style="color:#0000FF">Contact Us</th>
+  </tr>
+  <tr>
+    <td width="121" height="37">Name</td>
+    <td width="252">Ankit Kesarwani </td>
+  </tr>
+  <tr>
+    <td height="36">Mobile </td>
+    <td> +91-8349980190 </td>
+  </tr>
+  <tr>
+    <td>Address</td>
+    <td>C 51, Sector 30 Noida (U.P)</td>
+  </tr>
+    <tr>
+    <td>Email</td>
+    <td>kesarwani.ankit48@gmail.com</td>
+  </tr>
+   <tr>
+    <td>Website</td>
+    <td><a href="www.cslearning.com">www.cslearning.com</a></td>
+  </tr>
+  
+</table>
